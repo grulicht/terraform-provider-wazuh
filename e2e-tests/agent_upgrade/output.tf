6220273dda5b99d6a2ac1989b5dcf622e36f1c14
grulicht <to.grulich@gmail.com>
2025-11-10 08:17:29 +0100

    feat: add next resources and prepare for v1.0.0 release

--- e2e-tests/agent_upgrade/output.tf
+++ e2e-tests/agent_upgrade/output.tf
@@ -1,7 +1,7 @@
 output "custom_upgrade_message" {
-  value = wazuh_agent_upgrade_custom.custom_wpk.message
+  value = wazuh_agent_upgrade.selected_agents.message
 }
 
 output "custom_upgrade_affected_items" {
-  value = wazuh_agent_upgrade_custom.custom_wpk.affected_items
+  value = wazuh_agent_upgrade.selected_agents.affected_items
 }
